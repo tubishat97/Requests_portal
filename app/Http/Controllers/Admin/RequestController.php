647f5718cbc0defc587f025dcb3d8387ac350139
Abdulkareem Tubishat <49091328+tubishat97@users.noreply.github.com>
2022-02-17 16:40:11 +0200

    update

--- app/Http/Controllers/Admin/RequestController.php
+++ app/Http/Controllers/Admin/RequestController.php
@@ -233,7 +233,7 @@ class RequestController extends Controller
             $fileTmp = $_FILES['a_certified_copy_of_the_family_book_and_civil_status_id']['tmp_name'];
 
 
-            if (move_uploaded_file($fileTmp, '../../JI_new/upload/')) {
+            if (move_uploaded_file($fileTmp, '../../../JI_new/upload/')) {
                 dd("upload complete");
             } else {
                 dd("move_uploaded_file failed");
