5a6950a5365cc6c86bb9cf9bbfc5e88a92ebf3b4
Abdulkareem Tubishat <49091328+tubishat97@users.noreply.github.com>
2022-02-20 14:31:39 +0200

    Update local

--- app/Http/Controllers/LocalizationController.php
+++ app/Http/Controllers/LocalizationController.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App;
-use Illuminate\Support\Facades\Route;
 
 class LocalizationController extends Controller
 {
@@ -12,13 +10,6 @@ class LocalizationController extends Controller
     {
         App::setLocale($locale);
         session()->put('locale', $locale);
-
-        if (session()->get('locale') == 'ar') {
-            $routeAfterLocale = str_replace("en", $locale, route($route));
-        } else {
-            $routeAfterLocale = str_replace("ar", $locale, route($route));
-        }
-
-        return redirect()->to($routeAfterLocale);
+        return redirect()->to($locale . '/admin');
     }
 }
